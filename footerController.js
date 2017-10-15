56606dc46351b6ce280acf93bd3740643bfaf0a0
tarunraj95 <tarunrajkhanna@gmail.com>
2017-10-15 23:46:37 +0530

    Delete footerController.js

--- footerController.js
+++
@@ -1,9 +0,0 @@
-app.controller("footerController", function($scope, footerFactory){
-    var promise = footerFactory.getFooter();
-    promise.then((data)=>{
-        var footerData = data;
-        $scope.result = footerData.data;
-    },(err)=>{
-        console.log("error has occured in controller");
-    });
-});
